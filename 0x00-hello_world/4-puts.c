3a7e7cf5931fe07c7a04fbb12ecbe5e52e5f8244
Fwaloux <kaaxelzi@gmail.com>
2022-07-06 06:53:31 -0700

    put task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 
-int main (void)
+/**
+ * main-fonction de depart
+ *
+ * return: renvoie toujour 0 (ok)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
