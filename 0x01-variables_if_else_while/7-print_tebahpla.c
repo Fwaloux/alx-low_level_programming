8979ddabc19026c878315f72bd20f463b5311d67
Fwaloux <kaaxelzi@gmail.com>
2022-07-08 03:15:11 -0700

    test task 5-6-7-8-9

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char za;
+
+for (za = 'z'; za >= '9'; za--)
+
+	putchar(za);
+
+	putchar('\n');
+
+return (0);
+
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char car;
+int n;
+
+for (n = '0'; n <= '9'; n++)
+	putchar(d);
+for (car = 'a'; car <= 'f'; car++)
+	putchar(car);
+putchar('\n');
+
+return (0);
+}
